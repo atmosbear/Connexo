f09ed362d63e1d93db8fd7f52adfcfa121081724
atmosbear <mondegreen84@protonmail.com>
2023-01-25 19:22:43 -0800

    Added comments describing entry tests; made shortcut for console.assert (ensure)

--- index.js
+++ index.js
@@ -15,15 +15,29 @@ function entry(title) {
     return newEntry
   }
 }
-function test_entry() {
-  console.assert(entries.length === 0, "Entries is not empty at the start of the test!")
-  const helloEntry = entry("hello")
-  console.assert(entries.includes(helloEntry) && entries.length === 1, "The new entry didn't get added to the global array!")
-  // entries should not be duplicated, instead return the same object
-  const helloEntry2 = entry("hello")
-  console.assert(entries.length !== 2, "The entry was added twice!")
-  console.assert(entries.length === 1, "There's something more in the array!")
+
+function runTests() {
+  /**
+   * A shortcut for "console.assert".
+   * @param {boolean} what 
+   * @param {string} message 
+   */
+  function ensure(what, message) {
+    console.assert(what, message)
+  }
+  function test_entry() {
+    // the entries array should be clean.
+    entries.length = 0
+    // ensure that the entry gets added to the global array
+    const helloEntry = entry("hello")
+    ensure(entries.includes(helloEntry) && entries.length === 1, "The new entry didn't get added to the global array!")
+    // entries should not be duplicated, instead return the same object
+    const helloEntry2 = entry("hello")
+    ensure(entries.length !== 2, "The entry was added twice!")
+    ensure(entries.length === 1, "The length should be one, but it's " + entries.length)
+  }
+  test_entry()
 }
 
 const entries = []
-test_entry()
+runTests()
